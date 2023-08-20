4dd615a8aeff7d57981cdcb14d0bafae1f14f23b
Neal Morton <mortonne@gmail.com>
2023-08-20 10:51:39 -0500

    refactor: use jinja to produce bibliography entries

--- src/haml/publications.py
+++ src/haml/publications.py
@@ -9,10 +9,12 @@ from citeproc import CitationStylesStyle, CitationStylesBibliography
 from citeproc import Citation, CitationItem
 from citeproc import formatter
 from citeproc.source.json import CiteProcJSON
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 def gen_publications(json_file, csl_file, icon_background, icon_default):
     """Generate a publications list with icons and links."""
+    # get citation style and bibliography
     bib_style = CitationStylesStyle(csl_file)
     with open(json_file) as f:
         bib_data = json.load(f)
@@ -30,6 +32,14 @@ def gen_publications(json_file, csl_file, icon_background, icon_default):
         key: bib_source[key] for key, _ in sorted(bib_sort_keys.items(), key=lambda x: x[1], reverse=True)
     }
     
+    # set up templating engine
+    env = Environment(
+        loader=FileSystemLoader("templates"),
+        autoescape=select_autoescape(),
+    )
+    template = env.get_template("bib_entry.html")
+    
+    # generate bibliography
     year = None
     for key, val in bib_sorted.items():
         if "issued" in val and "year" in val["issued"]:
@@ -38,11 +48,15 @@ def gen_publications(json_file, csl_file, icon_background, icon_default):
                 year = new_year
                 print(f'# {year}')
         
+        # generate one-item bibliography for this entry
         citation = Citation([CitationItem(key)])
         bibliography = CitationStylesBibliography(bib_style, bib_source, formatter.html)
         bibliography.register(citation)
         for item in bibliography.bibliography():
+            reference = str(item)
+          
             if "icon" in val:
+                # generate custom icon
                 icon_path = Path(str(val["icon"]))
                 comb_name = str(icon_path.stem) + "_combined" + icon_path.suffix
                 comb_path = icon_path.parent / comb_name
@@ -51,29 +65,28 @@ def gen_publications(json_file, csl_file, icon_background, icon_default):
                 box = (np.array(img1.size) - np.array(img2.size)) / 2
                 img1.paste(img2, tuple(box.astype(int)), mask=img2)
                 img1.save(comb_path)
-                pic_file = str(comb_path)
+                icon = str(comb_path)
             else:
-                pic_file = icon_default
+                # use generic icon
+                icon = icon_default
             
-            print(f'<div class="container"><img class="icon" src="{pic_file}"></img><p class="bib-entry">{str(item)}', end='')
-            
+            # prepare links
+            links = {}
             if "DOI" in val:
                 doi = val["DOI"]
-                print(f' <a href="http://doi.org/{doi}">DOI</a>', end='')
+                links["DOI"] = f"http://doi.org/{doi}"
             if "link" in val:
-                link = val["link"]
-                print(f' <a href="{link}">Link</a>', end='')
+                links["link"] = val["link"]
             if "PMID" in val:
                 pmid = val["PMID"]
-                print(f' <a href="https://pubmed.ncbi.nlm.nih.gov/{pmid}">PubMed</a>', end='')
+                links["PMID"] = f"https://pubmed.ncbi.nlm.nih.gov/{pmid}"
             if "code" in val:
-                link = val["code"]
-                print(f' <a href="{link}">Code</a>', end='')
+                links["code"] = val["code"]
             if "data" in val:
-                link = val["data"]
-                print(f' <a href="{link}">Data</a>', end='')
+                links["data"] = val["data"]
             if "post" in val:
-                link = val["post"]
-                print(f' <a href="{link}">Post</a>', end='')
+                links["post"] = val["post"]
             
-            print('</p></div>\n\n')
+            # generate entry from template
+            entry = {"icon": icon, "reference": reference, "links": links}
+            print(template.render(entry=entry))
